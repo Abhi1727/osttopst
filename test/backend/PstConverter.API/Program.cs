233a45bee9d892475bc40bcc08631b36c8f98a90
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2026-02-21 18:00:03 +0530

    Migrate database from MySQL to SQL Server LocalDB

--- test/backend/PstConverter.API/Program.cs
+++ test/backend/PstConverter.API/Program.cs
@@ -30,11 +30,10 @@ builder.Services.AddEndpointsApiExplorer();// This is for endpoints api explorer
 builder.Services.AddSwaggerGen();// This is for swagger gen
 builder.Services.AddOpenApi();
 
-// MySQL Database Configuration
+// SQL Server Database Configuration
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var serverVersion = ServerVersion.AutoDetect(connectionString); // Detect once at startup
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, serverVersion));
+    options.UseSqlServer(connectionString));
 
 // Clerk Authentication
 var clerkConfig = builder.Configuration.GetSection("Clerk");
