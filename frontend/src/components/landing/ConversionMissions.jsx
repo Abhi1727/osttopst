ea36a95a8e3662764f29f5e6e3850b99440bc658
shivanshuofficials <shivanshuchauhanofficials@gmail.com>
2026-04-17 12:31:08 +0530

    init

--- frontend/src/components/landing/ConversionMissions.jsx
+++ frontend/src/components/landing/ConversionMissions.jsx
@@ -10,22 +10,22 @@ const ConversionMissions = () => {
 
   const defaultAudiences = [
     {
-      title: "Data Migration Specialists",
+      title: <a href='https://osttopst.us/our-plans'>Data Migration Specialists</a>,
       description:
         "Great for data migration specialists who need to be able to easily migrate data from one mail platform & server to another with no data loss.",
     },
     {
-      title: "IT Administrators",
+      title: <a href='https://osttopst.us/our-plans'>IT Administrators</a>,
       description:
         "Great for IT Administrators managing multiple user accounts, it can quickly convert, move, and restore all OSTs quickly and accurately across an organization.",
     },
     {
-      title: "Outlook Users",
+      title: <a href='https://osttopst.us/our-plans'>Outlook Users</a>,
       description:
         "This product is useful for regular Outlook users whose mailbox cannot be accessed when the Exchange server is down or offline, and need to have access, recovery, or backup.",
     },
     {
-      title: "Small to Large Businesses",
+      title: <a href='https://osttopst.us/our-plans'>Small to Large Businesses</a>,
       description:
         "Whether you are a growing startup business or a very large, established corporation, our converter is designed and built for you and keeps pace with the ever-increasing demand for bulk mailbox migrations.",
     },
